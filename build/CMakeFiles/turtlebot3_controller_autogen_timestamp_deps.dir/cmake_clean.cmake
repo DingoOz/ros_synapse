8961e045d1d985dfd1d090b7e9e69157f0ef9040
DingoOz <dingo.australia@gmail.com>
2025-07-03 22:41:34 +1000

    fixing the build folder inclusion

--- build/CMakeFiles/turtlebot3_controller_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/turtlebot3_controller_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/turtlebot3_controller_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
